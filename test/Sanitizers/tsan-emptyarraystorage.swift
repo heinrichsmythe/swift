c5dab03a45c8a80b9c309185a50d47c91605844c
Julian Lettner <jlettner@apple.com>
2019-05-13 17:14:12 -0700

    [TSan] Add positive test for TSan + Dispatch on Linux
    1) Enable tests that use `import Dispatch` on Linux. Add substitution
       `%import-libdispatch` that needs to be used for all cross-platform
       tests (i.e., tests that are intended to be run on other platforms
       than Darwin) that do `import Dispatch` or enable thread sanitizer.
    
    2) Make sure as many existing Dispatch and TSan tests as possible run on
       Linux. Mark tests that would require substantial work with
       `UNSUPPORTED: OS=linux-gnu`.
    
    3) Add test that shows that TSan finds a simple race when using
       `Dispatch.async` incorrectly. The real test suite for TSan's
       libdispatch support lives in the LLVM/compiler-rt side. Add
       `%deflake` substitution for deflake utility.
    
    rdar://problem/49177535

--- test/Sanitizers/tsan-emptyarraystorage.swift
+++ test/Sanitizers/tsan-emptyarraystorage.swift
@@ -1,4 +1,4 @@
-// RUN: %target-swiftc_driver %s -target %sanitizers-target-triple -g -sanitize=thread -o %t_tsan-binary
+// RUN: %target-swiftc_driver %s -target %sanitizers-target-triple -g -sanitize=thread %import-libdispatch -o %t_tsan-binary
 // RUN: %target-codesign %t_tsan-binary
 // RUN: %target-run %t_tsan-binary 2>&1 | %FileCheck %s
 // REQUIRES: executable_test
@@ -9,29 +9,22 @@
 // don't support TSan.
 // UNSUPPORTED: remote_run
 
-import Foundation
+import Dispatch
 
 let sem = DispatchSemaphore(value: 0)
+let q1 = DispatchQueue(label: "q1")
+let q2 = DispatchQueue(label: "q2")
 
-class T1: Thread {
-  override func main() {
-    var oneEmptyArray: [[String:String]] = []
-    oneEmptyArray.append(contentsOf: [])
-    sem.signal()
-  }
+q1.async {
+  var oneEmptyArray: [[String:String]] = []
+  oneEmptyArray.append(contentsOf: [])
+  sem.signal()
 }
-let t1 = T1()
-t1.start()
-
-class T2: Thread {
-  override func main() {
-    var aCompletelyUnrelatedOtherEmptyArray: [[Double:Double]] = []
-    aCompletelyUnrelatedOtherEmptyArray.append(contentsOf: [])
-    sem.signal()
-  }
+q2.async {
+  var aCompletelyUnrelatedOtherEmptyArray: [[Double:Double]] = []
+  aCompletelyUnrelatedOtherEmptyArray.append(contentsOf: [])
+  sem.signal()
 }
-let t2 = T2()
-t2.start()
 
 sem.wait()
 sem.wait()
